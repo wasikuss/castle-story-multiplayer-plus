2507cfad48c51dee256d449d80ce7a9d7cccd42a
wasikuss <wasikuss@gmail.com>
2023-01-20 12:46:26 +0100

    Add /team command to allow players to switch their teams

--- src/patches/LobbyPlayerPatch.cs
+++ src/patches/LobbyPlayerPatch.cs
@@ -29,6 +29,27 @@ namespace CastleStory_MultiplayerPlusPlugin
                     });
                 }
             }
+            else if (message.StartsWith("/team "))
+            {
+                string numPart = message.Substring(6);
+                int team = -1;
+                int.TryParse(numPart, out team);
+                if (team < 0 || team > 9)
+                {
+                    team = -1;
+                }
+                if (team == -1)
+                {
+                    string chatMessage = string.Format("<color=#{0}>Wrong team number. Valid team number is in range from 0 to 9.</color>", BaseMenu.HexCastleYellow);
+                    __instance.CallRpcServerNewChatMessage(chatMessage);
+                }
+                else
+                {
+                    __instance.CallCmdSetTeam(team);
+                    string chatMessage = string.Format("<color=#{0}>{1} has changed team to {2}.</color>", BaseMenu.HexCastleYellow, __instance.playerName, team);
+                    __instance.CallRpcServerNewChatMessage(chatMessage);
+                }
+            }
         }
     }
 }
