312174e27bda54bf1ff149784be4924996253b0c
wasikuss <wasikuss@gmail.com>
2023-01-20 12:42:48 +0100

    Write message for each of player with team he was assigned to

--- src/patches/LobbyPlayerPatch.cs
+++ src/patches/LobbyPlayerPatch.cs
@@ -6,17 +6,21 @@ namespace CastleStory_MultiplayerPlusPlugin
 {
     [HarmonyPatch(typeof(LobbyPlayer), nameof(LobbyPlayer.CmdSendChatMessage))]
     public class LobbyPlayerPatch
-    { 
+    {
+
         static void Prefix(LobbyPlayer __instance, string message)
         {
             if (__instance.hasAuthority && message.Equals("/teams"))
-			{
-				int team = 0;
-				UNetManager.instance.LobbyPlayerList.ForEach(delegate(LobbyPlayer lp)
-				{
-					lp.CallCmdSetTeam(team++);
-				});
-			}
+            {
+                int team = 0;
+                UNetManager.instance.LobbyPlayerList.ForEach(delegate (LobbyPlayer lp)
+                {
+                    lp.CallCmdSetTeam(team);
+                    string chatMessage = string.Format("<color=#{0}>{1} has changed team to {2}.</color>", BaseMenu.HexCastleYellow, lp.playerName, team);
+                    lp.CallRpcServerNewChatMessage(chatMessage);
+                    team++;
+                });
+            }
         }
     }
 }
